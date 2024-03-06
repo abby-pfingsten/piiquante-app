dc256d972a6e94e64abb5cb4d7038c44d48f9605
abby-pfingsten <abbypfin@yahoo.com>
2024-03-06 14:44:37 -0500

    image is saving but getting weird error

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,6 +8,6 @@ const sauceControl = require("../controllers/sauce");
 
 router.get("/", auth, sauceControl.getAllSauces);
 router.get("/:id", auth, sauceControl.getOneSauce);
-router.post("/:id", auth, multer, sauceControl.addOneSauce);
+router.post("/", auth, multer, sauceControl.addOneSauce);
 
 module.exports = router;
