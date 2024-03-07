565cc862128e5c7f02e0614ec06aa6a4845a7a4a
abby-pfingsten <abbypfin@yahoo.com>
2024-03-07 10:07:44 -0500

    removed packages from root, changing type of users like and dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -29,16 +29,22 @@ const Sauce = require("../models/sauce");
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({
-    _id: req.params.sauceid,
+    _id: req.params.id,
   })
     .then((sauce) => {
-      res.status(200).json(sauce);
+      if (sauce) {
+        res.status(200).json(sauce);
+      } else {
+        res.status(404).json({
+          error: "Sauce not found",
+        });
+      }
     })
     .catch((error) => {
       console.log(error);
 
       res.status(404).json({
-        error: error,
+        error: error.message,
       });
     });
 };
@@ -78,6 +84,7 @@ exports.addOneSauce = (req, res, next) => {
       });
     })
     .catch((error) => {
+      console.log(error);
       res.status(400).json({
         error: error.message,
       });
